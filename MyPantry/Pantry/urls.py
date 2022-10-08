08acd278f572a892ec230ae6bfe2fa72006faacd
no-bugs-only-features <dwright2@mail.csuchico.edu>
2022-10-07 20:27:36 -0700

    Added webpage views and urls.

--- MyPantry/Pantry/urls.py
+++ MyPantry/Pantry/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
 
+#You will need to change some of these urls to be dynamic.
+#https://docs.djangoproject.com/en/4.0/topics/http/urls/
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search/', views.search, name='search'),
+    path('login/', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('pantry/', views.pantry, name='pantry'),
+    path('browse/', views.browse, name='browse'),
+    path('viewRecipe/', views.viewRecipe, name='view'),
+    path('myRecipes/', views.myRecipes, name='saved'),
+    path('createRecipe/', views.createRecipe, name='create'),
+    path('updateRecipe/', views.updateRecipe, name='update'),
+    path('deleteRecipe/', views.deleteRecipe, name='delete'),
 ]
